ed8a3369051844ecf3c5f0b00da6fe12a3a1958c
nidhi1608 <nidhi.1608@gmail.com>
2014-09-22 01:22:36 -0700

    Initial Commit
    App demonstrating styles and themes in android applications.

---
+++ src/com/codepath/themeswitcherdemo/ThemeApplication.java
@@ -0,0 +1,7 @@
+package com.codepath.themeswitcherdemo;
+
+import android.app.Application;
+
+public class ThemeApplication extends Application {
+	public static int currentPosition;
+}
---
+++ src/com/codepath/themeswitcherdemo/Utils.java
@@ -0,0 +1,35 @@
+package com.codepath.themeswitcherdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class Utils {
+	private static int sTheme;
+
+	public final static int THEME_HOLO_LIGHT = 0;
+	public final static int THEME_CONSOLE = 1;
+	public final static int THEME_NAVY = 2;
+
+	public static void changeToTheme(Activity activity, int theme) {
+		sTheme = theme;
+		activity.finish();
+		activity.startActivity(new Intent(activity, activity.getClass()));
+		activity.overridePendingTransition(android.R.anim.fade_in,
+				android.R.anim.fade_out);
+	}
+
+	public static void onActivityCreateSetTheme(Activity activity) {
+		switch (sTheme) {
+		default:
+		case THEME_HOLO_LIGHT:
+			activity.setTheme(R.style.Theme_Holo_Light);
+			break;
+		case THEME_CONSOLE:
+			activity.setTheme(R.style.Theme_Console);
+			break;
+		case THEME_NAVY:
+			activity.setTheme(R.style.Theme_Navy);
+			break;
+		}
+	}
+}
